3730f5683dde9816bc6b8ca0dee7e4131eea1022
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-16 19:16:36 -0700

    fix + concat rather than adding, make calculator work & show on display

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const add = function (a, b) {
-    return a + b;
+    return +a + +b;
 };
 
 const subtract = function (a, b) {
-    return a - b;
+    return +a - +b;
 };
 
 const multiply = function (a, b) {
-    return a * b;
+    return +a * +b;
 };
 
 const divide = function (a, b) {
-    return a / b;
+    return +a / +b;
 };
 
 function operate(operator, a, b) {
@@ -26,15 +26,34 @@ function operate(operator, a, b) {
     }
 }
 
-let display = document.querySelector('.display')
-let displayVal = ''
+let display = document.querySelector('.display');
+let displayVal = '';
 function changeDisplay(val) {
     displayVal += val;
     display.textContent = displayVal;
 }
 
 
-numbers = document.querySelectorAll('.number')
-numbers.forEach(number => number.addEventListener('click', function() {
-    changeDisplay(this.textContent)
-}))
+numbers = document.querySelectorAll('.number');
+numbers.forEach(number => number.addEventListener('click', function () {
+    changeDisplay(this.textContent);
+}));
+
+let currentOperator = '';
+let firstNum = 0;
+let secondNum = 0;
+operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', function () {
+    currentOperator = this.textContent;
+    firstNum = displayVal;
+    displayVal = '';
+}));
+
+equals = document.querySelector('.equals');
+equals.addEventListener('click', function () {
+    secondNum = displayVal;
+    displayVal = firstNum = operate(currentOperator, firstNum, secondNum);
+    display.textContent = displayVal;
+});
+
+
